114ccce0ebf80cd242b88e9e8eb56ed6448b650e
frankgarciav <60076226+frankgarciav@users.noreply.github.com>
2020-04-07 01:39:01 -0500

    Dia 1

---
+++ scripts/tema1/04-calculadora ejemplo.R
@@ -0,0 +1,42 @@
+2*(3+5/2)
+2^-4
+pi
+2*pi
+Inf
+-Inf
+5/0
+0/0
+2^50
+c(2^30, 2^-15, 1, 2, 3/2)
+sqrt(9)
+log(exp(1))
+log(1000)
+log(1000,10)
+log10(1000)
+factorial(4)
+factorial(-4)
+chosse(5,3)
+choose(5,3)
+
+cos(60)
+
+x=(pi^2)/2
+x
+
+y<- cos(pi/4)
+y
+
+sin(pi/4) + cos(pi/4) -> z
+z
+
+edad <- 30
+
+x=sqrt(x)
+x
+
+miVariable = 4
+doble = function(x){2*x}
+doble(miVariable)
+
+cuadrado = function(x){x^2}
+cuadrado(miVariable)
